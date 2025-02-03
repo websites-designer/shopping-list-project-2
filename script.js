6136ac3e670a93d069e09c1da6bcbeb50bc2ab5a
Omar <oh.abdelkader2009@gmail.com>
2025-02-03 10:07:34 +0300

    adding the functions for x button and clear button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
 
 function addItem(e) {
   e.preventDefault();
@@ -13,25 +14,39 @@ function addItem(e) {
   // Create list item
   const li = document.createElement('li');
   li.appendChild(document.createTextNode(newItem));
-  const button = createButton('remove-item btn-link text-red'); 
+  const button = createButton('remove-item btn-link text-red');
   li.appendChild(button);
   itemList.appendChild(li);
   itemInput.value = '';
 }
 
 function createButton(classes) {
-    const button = document.createElement('button');
-    button.className = classes;
-    const icon = createIcon('fa-solid fa-xmark');
-    button.appendChild(icon);
-    return button ;
+  const button = document.createElement('button');
+  button.className = classes;
+  const icon = createIcon('fa-solid fa-xmark');
+  button.appendChild(icon);
+  return button;
 }
 
-function createIcon(classes){
-    const icon = document.createElement('i');
-    icon.className = classes ;
-    return icon ;
+function createIcon(classes) {
+  const icon = document.createElement('i');
+  icon.className = classes;
+  return icon;
+}
+
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+function clearAll(e){
+  while(itemList.firstChild){
+    itemList.firstChild.remove()
+  }
 }
 
 // Event Listener
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearAll);
