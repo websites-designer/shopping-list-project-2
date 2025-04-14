442d1bc79d319f471b158c3edd4fe953f8dfcd68
Omar <oh.abdelkader2009@gmail.com>
2025-04-14 16:12:23 +0300

    linked my project with local storage

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ const itemList = document.getElementById('item-list');
 const clearButton = document.getElementById('clear');
 const itemFilter = document.getElementById('filter');
 
+function displayItems(){
+  const itemsFromStorage = getItemFromStorage();
+  itemsFromStorage.forEach(item => addItemToDOM(item))
+}
 
 
-function addItem(e) {
+function OnAddItemSubmit(e) {
   e.preventDefault();
   const newItem = itemInput.value;
   // Validate Input
@@ -15,24 +19,58 @@ function addItem(e) {
     return;
   }
 
+  addItemToDOM(newItem);
+  addItemToStorage(newItem);
+  checkUI();
+}
+
+function addItemToDOM(item) {
   // Create list item
 
   const li = document.createElement('li');
-  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(item));
   const button = createButton('remove-item btn-link text-red');
   li.appendChild(button);
   itemList.appendChild(li);
 
   itemInput.value = '';
-  checkUI();
 }
 
-function removeItem(e) {
-  if (e.target.parentElement.classList.contains('remove-item')) {
-    if(confirm('Are you sure you want to delete the element ? ')){
-    e.target.parentElement.parentElement.remove();
-    }
+function addItemToStorage(item) {
+  const itemsFromStorage = getItemFromStorage();
+  itemsFromStorage.push(item);
+  localStorage.setItem('items', JSON.stringify(itemsFromStorage));
 }
+
+function getItemFromStorage() {
+  let itemsFromStorage;
+  if (localStorage.getItem('items') === null) {
+    itemsFromStorage = [];
+  } else {
+    itemsFromStorage = JSON.parse(localStorage.getItem('items'));
+  }
+
+  return itemsFromStorage ;
+ 
+}
+
+function onClickItem(e){
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    removeItem(e.target.parentElement.parentElement)
+    removeItemFromStorage(e.target.parentElement.parentElement)
+  }
+}
+
+function removeItemFromStorage(removedItem){
+  const itemsFromStorage = getItemFromStorage();
+  const filteredItems = itemsFromStorage.filter(item => item !== removedItem.firstChild.textContent)
+  localStorage.setItem('items' , JSON.stringify(filteredItems));
+}
+
+function removeItem(item) {
+    if (confirm('Are you sure you want to delete the element ? ')) {
+      item.remove();
+    }
   checkUI();
 }
 
@@ -40,18 +78,18 @@ function clearItems() {
   while (itemList.firstChild) {
     itemList.firstChild.remove();
   }
-  checkUI()
+  localStorage.removeItem('items');
+  checkUI();
 }
 
-function filterItems(e){
-    const items = Array.from(itemList.querySelectorAll('li'));
-    const text = e.target.value.toLowerCase() ;
-    items 
-  .forEach(ele => {
-    if(ele.firstChild.textContent.toLowerCase().includes(text)){
-        ele.style.display = 'flex'
-    }else{
-        ele.style.display = 'none'
+function filterItems(e) {
+  const items = Array.from(itemList.querySelectorAll('li'));
+  const text = e.target.value.toLowerCase();
+  items.forEach((ele) => {
+    if (ele.firstChild.textContent.toLowerCase().includes(text)) {
+      ele.style.display = 'flex';
+    } else {
+      ele.style.display = 'none';
     }
   });
 }
@@ -71,21 +109,22 @@ function createIcon(classes) {
 }
 
 function checkUI() {
-const items = itemList.querySelectorAll('li');
+  const items = itemList.querySelectorAll('li');
 
-if (items.length === 0) {
-      clearButton.style.display = 'none';
-      itemFilter.style.display = 'none';
-}else{
+  if (items.length === 0) {
+    clearButton.style.display = 'none';
+    itemFilter.style.display = 'none';
+  } else {
     clearButton.style.display = 'block';
     itemFilter.style.display = 'block';
-}
+  }
 }
 
 // Event Listeners
 
-itemForm.addEventListener('submit', addItem);
-itemList.addEventListener('click', removeItem);
+itemForm.addEventListener('submit', OnAddItemSubmit);
+itemList.addEventListener('click', onClickItem);
 clearButton.addEventListener('click', clearItems);
-itemFilter.addEventListener('input', filterItems)
+itemFilter.addEventListener('input', filterItems);
+displayItems();
 checkUI();
